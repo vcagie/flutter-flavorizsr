115e0a8e2617c82edce5237c8689056f1e49af2f
vernando.cahyadi <vernando.cahyadi@commsult.id>
2022-07-06 11:40:59 +0700

    add sh file

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+# Get all packages
+# flutter packages get
+
+# # Check available devices 
+# flutter devices
+
+# # Build Release
+# declare -a arr=("client1" "client2" "client3")
+
+# for i in "${arr[@]}"
+# do
+#     flutter build apk --release --flavor $i -t lib/main_$i.dart
+# done
+
+pwd;
